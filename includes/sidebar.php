28c91966460dfe0159a4f406044fb33939964147
Zrinka <60172572+zkomic@users.noreply.github.com>
2024-02-26 12:49:50 +0100

    sidebar_categories

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -17,32 +17,43 @@
     </div>
 
     <!-- Blog Categories Well -->
+
+    <?php
+
+    $query = "SELECT * FROM categories";
+    //$query = "SELECT * FROM categories LIMIT 2";
+    $categories = mysqli_query($connection, $query);
+
+    ?>
+
     <div class="well">
         <h4>Blog Categories</h4>
         <div class="row">
             <div class="col-lg-6">
                 <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
+
+                    <?php
+
+                    while ($row = mysqli_fetch_assoc($categories)) {
+
+                        $cat_title = $row['cat_title'];
+                        echo "<li><a href='#'>{$cat_title}</a></li>";
+                    }
+                    ?>
+
                 </ul>
             </div>
             <!-- /.col-lg-6 -->
             <div class="col-lg-6">
                 <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
+                    <!-- <li><a href="#">Category Name</a>
                     </li>
                     <li><a href="#">Category Name</a>
                     </li>
                     <li><a href="#">Category Name</a>
                     </li>
                     <li><a href="#">Category Name</a>
-                    </li>
+                    </li> -->
                 </ul>
             </div>
             <!-- /.col-lg-6 -->
@@ -51,9 +62,6 @@
     </div>
 
     <!-- Side Widget Well -->
-    <div class="well">
-        <h4>Side Widget Well</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-    </div>
+    <?php include "widget.php" ?>
 
 </div>
