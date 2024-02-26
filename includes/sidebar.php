a352a47a1493e1aeea9f78fad30334599c9fbd7e
Zrinka <60172572+zkomic@users.noreply.github.com>
2024-02-26 12:32:46 +0100

    search

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,14 +3,16 @@
     <!-- Blog Search Well -->
     <div class="well">
         <h4>Blog Search</h4>
-        <div class="input-group">
-            <input type="text" class="form-control">
-            <span class="input-group-btn">
-                <button class="btn btn-default" type="button">
-                    <span class="glyphicon glyphicon-search"></span>
-                </button>
-            </span>
-        </div>
+        <form action="search.php" method="post">
+            <div class="input-group">
+                <input type="text" name="search" class="form-control">
+                <span class="input-group-btn">
+                    <button class="btn btn-default" name="submit" type="submit">
+                        <span class="glyphicon glyphicon-search"></span>
+                    </button>
+                </span>
+            </div>
+        </form>
         <!-- /.input-group -->
     </div>
 
