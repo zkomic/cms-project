66cbb62fe5fd12e840af986dd1db8eb31954b514
Zrinka <60172572+zkomic@users.noreply.github.com>
2024-02-26 10:20:29 +0100

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,17 @@
-<?php 
+<?php
 
-$connection = mysqli_connect('localhost','root','','cms');
+$db['db_host'] = "localhost";
+$db['db_user'] = "root";
+$db['db_password'] = "";
+$db['db_name'] = "cms";
 
-if($connection) {
-    echo "We are connected!";
+foreach ($db as $key => $value) {
+
+    define(strtoupper($key), $value);
 }
 
-?>
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+if ($connection) {
+    echo "We are connected!";
+}
