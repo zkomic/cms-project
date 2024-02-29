e14cc760726b0754f203c1cd1781b8ea7510dc0f
Zrinka <60172572+zkomic@users.noreply.github.com>
2024-02-29 10:53:03 +0100

    dashboard_counts

--- admin/index.php
+++ admin/index.php
@@ -53,6 +53,13 @@
 
                                     $posts_count = mysqli_num_rows($posts);
 
+                                    $query = "SELECT * FROM posts WHERE post_status = 'draft'";
+                                    $posts_draft = mysqli_query($connection, $query);
+                                    queryTest($posts_draft);
+
+                                    $posts_draft_count = mysqli_num_rows($posts_draft);
+                                    $posts_active_count = $posts_count - $posts_draft_count;
+
                                     ?>
 
                                     <div class='huge'><?php echo $posts_count; ?></div>
@@ -87,12 +94,12 @@
 
                                     $comments_count = mysqli_num_rows($comments);
 
-                                    // $query = "SELECT * FROM comments WHERE comment_status = 'approved'";
-                                    // $comments_approved = mysqli_query($connection, $query);
-                                    // queryTest($comments_approved);
+                                    $query = "SELECT * FROM comments WHERE comment_status = 'approved'";
+                                    $comments_approved = mysqli_query($connection, $query);
+                                    queryTest($comments_approved);
 
-                                    // $comments_approved_count = mysqli_num_rows($comments_approved);
-                                    // $comments_unapproved_count = $comments_count - $comments_approved_count;
+                                    $comments_approved_count = mysqli_num_rows($comments_approved);
+                                    $comments_unapproved_count = $comments_count - $comments_approved_count;
 
                                     ?>
 
@@ -126,6 +133,13 @@
                                     queryTest($users);
 
                                     $users_count = mysqli_num_rows($users);
+
+                                    $query = "SELECT * FROM users WHERE user_role = 'admin'";
+                                    $users_admin = mysqli_query($connection, $query);
+                                    queryTest($users_admin);
+
+                                    $users_admin_count = mysqli_num_rows($users_admin);
+                                    $users_subs_count = $users_count - $users_admin_count;
 
                                     ?>
 
@@ -178,11 +192,105 @@
                 </div>
             </div>
             <!-- /.row -->
+            <div class="row">
+                <script type="text/javascript">
+                    google.charts.load('current', {
+                        'packages': ['bar']
+                    });
+                    google.charts.setOnLoadCallback(drawChart1);
 
+                    function drawChart1() {
+                        var postsCount = <?php echo $posts_count; ?>;
+                        var postsActiveCount = <?php echo $posts_active_count; ?>;
+                        var postsDraftCount = <?php echo $posts_draft_count; ?>;
+
+                        var data1 = google.visualization.arrayToDataTable([
+                            ['', 'All Posts', 'Published', 'Not Published'],
+                            ['Posts', postsCount, postsActiveCount, postsDraftCount],
+                        ]);
+
+                        var options1 = {
+                            chart: {
+                                title: '',
+                                subtitle: '',
+                            },
+                        };
+
+                        var chart1 = new google.charts.Bar(document.getElementById('chart1'));
+
+                        chart1.draw(data1, google.charts.Bar.convertOptions(options1));
+                    }
+                </script>
+            </div>
+            <div class="row">
+                <script type="text/javascript">
+                    google.charts.load('current', {
+                        'packages': ['bar']
+                    });
+                    google.charts.setOnLoadCallback(drawChart2);
+
+                    function drawChart2() {
+                        var commentsCount = <?php echo $comments_count; ?>;
+                        var commentsActiveCount = <?php echo $comments_approved_count; ?>;
+                        var commentsDraftCount = <?php echo $comments_unapproved_count; ?>;
+
+                        var data2 = google.visualization.arrayToDataTable([
+                            ['', 'All Comments', 'Approved', 'Unapproved'],
+                            ['Comments', commentsCount, commentsActiveCount, commentsDraftCount],
+                        ]);
+
+                        var options2 = {
+                            chart: {
+                                title: '',
+                                subtitle: '',
+                            },
+                        };
+
+                        var chart2 = new google.charts.Bar(document.getElementById('chart2'));
+
+                        chart2.draw(data2, google.charts.Bar.convertOptions(options2));
+                    }
+                </script>
+            </div>
+            <div class="row">
+                <script type="text/javascript">
+                    google.charts.load('current', {
+                        'packages': ['bar']
+                    });
+                    google.charts.setOnLoadCallback(drawChart3);
+
+                    function drawChart3() {
+                        var usersCount = <?php echo $users_count; ?>;
+                        var usersAdminCount = <?php echo $users_admin_count; ?>;
+                        var usersSubsCount = <?php echo $users_subs_count; ?>;
+
+                        var data3 = google.visualization.arrayToDataTable([
+                            ['', 'All Users', 'Admins', 'Subscribers'],
+                            ['Users', usersCount, usersAdminCount, usersSubsCount],
+                        ]);
+
+                        var options3 = {
+                            chart: {
+                                title: '',
+                                subtitle: ''
+                            },
+                        };
+
+                        var chart3 = new google.charts.Bar(document.getElementById('chart3'));
+
+                        chart3.draw(data3, google.charts.Bar.convertOptions(options3));
+                    }
+                </script>
+                <div id="chart1" style="width: 33%; height: 500px; display: inline-block;"></div>
+                <div id="chart2" style="width: 33%; height: 500px; display: inline-block;"></div>
+                <div id="chart3" style="width: 33%; height: 500px; display: inline-block;"></div>
+            </div>
         </div>
-        <!-- /.container-fluid -->
 
     </div>
-    <!-- /#page-wrapper -->
+    <!-- /.container-fluid -->
 
-    <?php include "includes/admin_footer.php" ?>
+</div>
+<!-- /#page-wrapper -->
+
+<?php include "includes/admin_footer.php" ?>
