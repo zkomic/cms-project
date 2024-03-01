482cf819f915a6eb88a91369a765a8aa9f4328b5
Zrinka <60172572+zkomic@users.noreply.github.com>
2024-03-01 09:09:58 +0100

    loader

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -39,6 +39,7 @@ if (!isset($_SESSION['role'])) {
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <link href="css/styles.css" rel="stylesheet">
 
     <!-- Text Editor summernote css -->
     <!-- <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet"> -->
